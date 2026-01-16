e8208d0738e1a220e0e29c2f4d8fa237f32b9ec2
Agustin Farisano <afarisano@bitbucket.org>
2026-01-16 13:16:49 -0300

    agrego interface para usar en propiedad de menu con for en html del componente y funcionalidad de rutas con routerlinkActive

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+
+interface menuOptions{
+  label:string,
+  sublabel:string,
+  route:string,
+  icon:string
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.html',
   styleUrl: './gifs-side-menu-options.css',
 })
 export class GifsSideMenuOptions {
+
+   menuOptions:menuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs populares',
+      route: 'trending'
+    },
+      {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar Gifs',
+      route: 'search'
+    }
+   ]
+item: any;
 
 }
