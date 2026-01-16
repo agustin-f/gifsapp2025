40dd2ca0fd1b5052e65e400e26d19962916001c2
Agustin Farisano <afarisano@bitbucket.org>
2026-01-16 12:03:44 -0300

    agrego componentes y rutas hijas para dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('../app/gifs/pages/dashboard-page/dashboard-page'),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('../app/gifs/pages/trending-page/trending-page'),
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('../app/gifs/pages/search-page/search-page'),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [RouterOutlet],
+  templateUrl: './dashboard-page.html',
+  styleUrl: './dashboard-page.css',
+})
+export default class DashboardPage {
+
+}
